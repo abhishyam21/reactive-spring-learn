242e117535099a4e9ed6b5f7b7af0b5a661e8ead
300069965 <abhishyam.chennayipalem@myntra.com>
2020-08-01 16:46:29 +0530

    first commit

---
+++ src/main/java/com/learning/reactivespring/ReactiveSpringLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learning.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/reactivespring/ReactiveSpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
