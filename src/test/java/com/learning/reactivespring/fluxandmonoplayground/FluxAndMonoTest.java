f6537c54fe410c58f97ab337b66ae717af89e467
300069965 <abhishyam.chennayipalem@myntra.com>
2020-08-01 17:15:44 +0530

    Base test cases with flux

--- src/test/java/com/learning/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/learning/reactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -2,6 +2,7 @@ package com.learning.reactivespring.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 /**
  * @author Abhishyam.c on 01/08/20
@@ -29,5 +30,51 @@ public class FluxAndMonoTest {
         stringFlux
                 .subscribe(System.out::println,
                         System.out::println);
+    }
+
+
+    @Test
+    public void testFluxElementsSequenceWithOnComplete(){
+        Flux<String> stringFlux = Flux
+                .just("Spring", "Spring Boot", "Reactive Spring")
+                .log();
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testFluxElementsSequenceWithException(){
+        Flux<String> stringFlux = Flux
+                .just("Spring", "Spring Boot", "Reactive Spring")
+                .concatWith(
+                        Flux.error(new RuntimeException("Exception Created by me"))
+                )
+                .log();
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Spring")
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    public void testFluxElementsSequenceWithException1(){
+        Flux<String> stringFlux = Flux
+                .just("Spring", "Spring Boot", "Reactive Spring")
+                .concatWith(
+                        Flux.error(new RuntimeException("Exception Created by me"))
+                )
+                .log();
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("Spring", "Spring Boot", "Reactive Spring")
+                .expectError(RuntimeException.class)
+                .verify();
     }
 }
