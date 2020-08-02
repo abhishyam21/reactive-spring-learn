24ed500b4681362fc41cabcb1382c25f7f2b679a
300069965 <abhishyam.chennayipalem@myntra.com>
2020-08-02 15:33:08 +0530

    Flux Factory methods

---
+++ src/test/java/com/learning/reactivespring/fluxandmonoplayground/FluxAndFlatMapTest.java
@@ -0,0 +1,12 @@
+package com.learning.reactivespring.fluxandmonoplayground;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Abhishyam.c on 02/08/20
+ */
+public class FluxAndFlatMapTest {
+    List<String> names = Arrays.asList("chiru", "nag", "venky", "balayya");
+
+}
