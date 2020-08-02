3af3d2a85831cb49040889d4f15fa9f8b4b7f872
300069965 <abhishyam.chennayipalem@myntra.com>
2020-08-02 15:12:58 +0530

    Flux Factory methods

--- src/test/java/com/learning/reactivespring/fluxandmonoplayground/FluxFactory.java
+++
@@ -1,7 +0,0 @@
-package com.learning.reactivespring.fluxandmonoplayground;
-
-/**
- * @author Abhishyam.c on 02/08/20
- */
-public class FluxFactory {
-}
---
+++ src/test/java/com/learning/reactivespring/fluxandmonoplayground/FluxFactoryTest.java
@@ -0,0 +1,36 @@
+package com.learning.reactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Abhishyam.c on 02/08/20
+ */
+public class FluxFactoryTest {
+
+    List<String> names = Arrays.asList("chiru", "nag", "venky", "balayya");
+
+    /**
+     * Crating a flux from list
+     */
+    @Test
+    public void fluxUsingIterator(){
+        Flux<String> namesFlux = Flux.fromIterable(names);
+        StepVerifier.create(namesFlux.log())
+                .expectNext("chiru", "nag", "venky", "balayya")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxUsingArray(){
+        String[] names = new String[]{"chiru", "nag", "venky", "balayya"};
+        Flux<String> namesFlux = Flux.fromArray(names);
+        StepVerifier.create(namesFlux)
+                .expectNext("chiru", "nag", "venky", "balayya")
+                .verifyComplete();
+    }
+}
